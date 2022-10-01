8241e5efabcdfc9a06ce150845c6b66102815f4c
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-10-01 08:26:08 +0000

    Update scalatest to 3.2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,6 +16,6 @@ object Dependencies {
   lazy val keycloakCore = "org.keycloak" % "keycloak-core" % keycloakVersion
   lazy val keycloakAdminClient =  "org.keycloak" % "keycloak-admin-client" % keycloakVersion
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.12"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock-jre8" % "2.34.0"
 }
