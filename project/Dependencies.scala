2f51e9f08350ab67761ef95c6d85693e43d290b3
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-12-04 08:28:10 +0000

    Update ssm to 2.21.37

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.180"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
-  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.21.36"
+  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.21.37"
   lazy val typeSafeConfig = "com.typesafe" % "config" % "1.4.3"
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.353"
   lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.140"
