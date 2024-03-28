f9936433b73beab7268b4ac711dc75b6e32a586e
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-03-28 08:26:33 +0000

    Update tdr-generated-graphql to 0.0.371 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.25.16"
   lazy val typeSafeConfig = "com.typesafe" % "config" % "1.4.3"
-  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.370"
+  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.371"
   lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.153"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
