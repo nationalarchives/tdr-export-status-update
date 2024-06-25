5996684c9b1dbf60f8df3c5fbf1c01029f3b1c9b
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-06-25 08:27:21 +0000

    Update mockito-scala to 1.17.37 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val keycloakCore = "org.keycloak" % "keycloak-core" % keycloakVersion
   lazy val keycloakAdminClient =  "org.keycloak" % "keycloak-admin-client" % keycloakVersion
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.31"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.37"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
   lazy val wiremockStandalone = "com.github.tomakehurst" % "wiremock-standalone" % "3.0.1"
 }
